88cc02dfc96feeb8ccf9ede5869570e74b07fb9d
Anvar <101347819+anvaraxadjonov1802@users.noreply.github.com>
2026-08-08 12:59:12 +0500

    feat: load automatic enrollment signals

--- backend/lms/apps.py
+++ backend/lms/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+
 class LmsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lms'
+
+    def ready(self):
+        # Load automatic enrollment signals once Django has initialized models.
+        from . import signals  # noqa: F401
